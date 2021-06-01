13e7aba7542c85d06b485c065e37004d97398e19
Julio Biason <julio.biason@pm.me>
2021-06-01 12:47:04 -0300

    Creating a repository is harder than you think

--- sqlxtest/src/main.rs
+++ sqlxtest/src/main.rs
@@ -1,6 +1,36 @@
 use sqlx::sqlite::SqliteConnectOptions;
 use sqlx::sqlite::SqlitePoolOptions;
+use sqlx::Pool;
 use std::env;
+
+struct Label {
+    id: u32,
+    description: String,
+}
+
+struct Repository<T>
+where
+    T: sqlx::Database,
+{
+    pool: Pool<T>,
+}
+
+impl<T> Repository<T>
+where
+    T: sqlx::Database,
+{
+    fn new(pool: Pool<T>) -> Self {
+        Self { pool }
+    }
+
+    async fn save(&mut self, label: &Label) -> Result<(), sqlx::Error> {
+        sqlx::query(r#"INSERT INTO testing (label) VALUES (?)"#)
+            .bind(label.description)
+            .execute(self.pool)
+            .await?;
+        Ok(())
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
@@ -14,15 +44,13 @@ async fn main() -> Result<(), sqlx::Error> {
         .await?;
     sqlx::migrate!("db/migrations").run(&pool).await?;
 
+    let repo = Repository::new(pool);
+
     let command = env::args().nth(1).unwrap();
     println!("Command: \"{}\"", command);
     if command == "add" {
         let value = env::args().nth(2).unwrap();
         println!("Should add \"{}\"", value);
-        sqlx::query(r#"INSERT INTO testing (label) VALUES (?)"#)
-            .bind(value)
-            .execute(&pool)
-            .await?;
     } else if command == "remove" {
         let value = env::args().nth(2).unwrap();
         println!("Should remove \"{}\"", value);
