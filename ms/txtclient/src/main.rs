9ba2af2daf33145ec7cbb0b9995f7ae3a8bb38f9
Julio Biason <julio.biason@pm.me>
2021-11-18 16:00:10 -0300

    A client that outputs what it receives

--- ms/txtclient/src/main.rs
+++ ms/txtclient/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::io::AsyncReadExt;
+use tokio::net::TcpStream;
+
+use shared::Message;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    env_logger::init();
+
+    let mut stream = TcpStream::connect("127.0.0.1:4435")
+        .await
+        .expect("Failed to connect to server");
+    loop {
+        let mut buffer = [0; 1024];
+        let bytes = stream.read(&mut buffer).await.expect("Failed to read data");
+        let decoded: Message =
+            bincode::deserialize(&buffer[..bytes]).expect("Failed to convert message");
+        log::info!("Got {:?}", decoded);
+    }
 }
