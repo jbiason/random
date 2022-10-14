960cc833dcf427ab86b973b1d459a4ee934e3abc
Julio Biason <j.biason@engys.com>
2022-10-14 15:44:18 -0300

    Splitting the parsers

--- pyparsers.py
+++ pyparsers.py
@@ -1,5 +1,11 @@
 import argparse
 
+def git_init(args):
+    print('Init collection:', args.collection)
+
+def git_sync(args):
+    print('Sync')
+
 def main():
     parser = argparse.ArgumentParser(description='Main')
     subparsers = parser.add_subparsers()
@@ -8,11 +14,19 @@ def main():
     git_subs = git.add_subparsers()
 
     init = git_subs.add_parser(name='init', help='Init')
+    init.set_defaults(func=git_init)
     init.add_argument('collection')
 
     sync = git_subs.add_parser(name='sync', help='Sync')
+    sync.set_defaults(func=git_sync)
 
     args = parser.parse_args()
+
+    if not hasattr(args, 'func'):
+        parser.print_usage()
+        return
+
+    args.func(args)
 
 
 if __name__ == '__main__':
