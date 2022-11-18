06dc3b3dc327fe0e5edd76b0be94702b8964d318
Julio Biason <j.biason@engys.com>
2022-11-18 16:03:38 -0300

    External subcommands is the key;

--- subcommand/src/main.rs
+++ subcommand/src/main.rs
@@ -1,8 +1,12 @@
+// This doesnt works:
+
 use clap::Arg;
 use clap::Command;
 
 fn main() {
     let command = Command::new("example")
+        .subcommand_required(true)
+        .allow_external_subcommands(true)
         .arg(
             Arg::new("working_directory")
                 .short('w')
@@ -18,8 +22,38 @@ fn main() {
 
     match matches.subcommand() {
         Some(("run", _sub_matches)) => println!("It's run"),
+        Some((ext, _sub_matches)) => println!("Finding if we have a \"{ext}\" command"),
         _ => {
             println!("Finding out if it an external command...")
         }
     }
 }
+
+// This doesn't work
+
+// use std::path::{Path, PathBuf};
+
+// use clap::{Parser, Subcommand};
+
+// #[derive(Parser)]
+// #[command(version)]
+// struct Cli {
+//     #[arg(short, long)]
+//     working_directory: Option<PathBuf>,
+
+//     #[command(subcommand)]
+//     command: Command,
+// }
+
+// #[derive(Subcommand)]
+// enum Command {
+//     Run { cases: Option<Vec<String>> },
+// }
+
+// fn main() {
+//     let cli = Cli::parse();
+//     match &cli.command {
+//         Command::Run { cases } => println!("Running cases: {:?}", cases),
+//         _ => println!("External"),
+//     }
+// }
